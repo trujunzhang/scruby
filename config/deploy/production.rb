00eea2c7811a14c2b4fc1a1904e96e85a1bb6a95
Trujun Zhang <trujunzhang@gmail.com>
2016-05-02 15:43:22 +0800

    Website and Social Media Data Entry.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,4 +3,4 @@ set :stage, :production
 # Replace 127.0.0.1 with your server's IP address!
 server '139.59.226.195', user: 'deploy', roles: %w{web app}
 
-# server '192.168.1.106', user: 'deploy', roles: %w{web app}
+server '192.168.1.106', user: 'deploy', roles: %w{web app}
