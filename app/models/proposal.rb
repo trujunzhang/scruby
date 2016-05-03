e74ebe254e096e763183c4f0cabcae1af982c5af
Trujun Zhang <trujunzhang@gmail.com>
2016-05-03 20:03:55 +0800

    Website and Social Media Data Entry.

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -2,9 +2,6 @@ class Proposal
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :jobid, type: String
   field :title, type: String
   field :content, type: String
-  field :createdat, type: String
-  field :updatedat, type: String
 end
