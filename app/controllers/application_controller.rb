fa6825c5e6ee54ec31f155249e2c8165265cf2f2
Trujun Zhang <trujunzhang@gmail.com>
2016-05-03 20:54:10 +0800

    Website and Social Media Data Entry.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,6 @@ require 'ItunesGenerator'
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  protected
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to login_path, :notice => 'if you want to add a notice'
-      ## if you want render 404 page
-      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
-    end
-  end
-
   def user_email
 
     @email= ""
@@ -89,6 +78,17 @@ class ApplicationController < ActionController::Base
       GooglePlaysGenerator.new("googleplays")
     elsif type == "itunes"
       ItunesGenerator.new("itunes")
+    end
+  end
+
+  protected
+  def authenticate_user!
+    if user_signed_in?
+      super
+    else
+      redirect_to login_path, :notice => 'if you want to add a notice'
+      ## if you want render 404 page
+      ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
   end
 
