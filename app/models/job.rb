239d08729828150c32b818cc7af9599434588ad7
Trujun Zhang <trujunzhang@gmail.com>
2016-05-03 22:07:44 +0800

    capd.

--- app/models/job.rb
+++ app/models/job.rb
@@ -6,6 +6,7 @@ class Job
   field :jobtype, type: String
   field :proposalid, type: String
   field :url, type: String
+  field :from, type: String
   field :customer, type: String
   field :description, type: String
   field :app, type: String
