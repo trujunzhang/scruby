409096d89b7d4617bacc3534ff8d80a2ca86a110
Trujun Zhang <trujunzhang@gmail.com>
2016-05-08 16:56:41 +0800

    capd.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,15 +35,6 @@ Scruby::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  # config.action_mailer.smtp_settings = {
-  #   address: "smtp.gmail.com",
-  #   port: 587,
-  #   domain: ENV["DOMAIN_NAME"],
-  #   authentication: "plain",
-  #   enable_starttls_auto: true,
-  #   user_name: ENV["GMAIL_USERNAME"],
-  #   password: ENV["GMAIL_PASSWORD"]
-  # }
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
