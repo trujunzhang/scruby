22e5271899235d61d30705c703bdcf64aab167fd
Trujun Zhang <trujunzhang@gmail.com>
2016-05-08 18:25:48 +0800

    capd.

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,6 +40,8 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
+set :whenever_identifier, ->{ "#{fetch(:application)}_#{fetch(:production)}" }
+
 namespace :deploy do
 
   after :restart, :clear_cache do
