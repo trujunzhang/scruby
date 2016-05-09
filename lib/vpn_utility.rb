69cf55303ebffb9a0ce26b887f75961bfcc517da
Trujun Zhang <trujunzhang@gmail.com>
2016-05-09 17:03:12 +0800

    capd.

--- lib/vpn_utility.rb
+++ lib/vpn_utility.rb
@@ -56,13 +56,13 @@ class VPNUtility
     _command = "nmcli con down uuid #{@lastUUID}"
     puts "command is #{_command}"
     if @lastUUID != "ffffffff"
-      exec "nmcli con down uuid #{@lastUUID}"
+      system "nmcli con down uuid #{@lastUUID}"
     end
 
     puts "Connected vpn uuid is #{@vpnUUID}"
     _command = "nmcli con up uuid #{@vpnUUID}"
     puts "command is #{_command}"
-    exec "nmcli con up uuid #{@vpnUUID}"
+    system "nmcli con up uuid #{@vpnUUID}"
   end
 
 
