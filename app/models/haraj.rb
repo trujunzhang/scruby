458242d483ee48cfc280dfe2c1fce906178bacde
djzhang <djzhang@djzhangs-MacBook-Pro.local>
2016-06-08 19:02:48 +0800

    search jobs form.

--- app/models/haraj.rb
+++ app/models/haraj.rb
@@ -1,5 +1,24 @@
 class Haraj
   include Mongoid::Document
-  field :title, type: String
+
+  field :guid, type: String
   field :url, type: String
+
+  field :ID, type: String
+  field :city, type: String
+  field :time, type: String
+  field :title, type: String
+  field :pictures, type: Array
+  field :subject, type: String
+  field :contact, type: String
+  field :number, type: String
+
+  # cache form where, such as opensooq,mstaml.(WebsiteTypes variable)
+  field :url_from, type: String
+
+  field :address, type: String
+  field :memberName, type: String
+  field :description, type: String
+  field :section, type: String
+
 end
